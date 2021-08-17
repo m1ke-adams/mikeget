377609aa625e551d214642cb70a268ebd49e9b78
Discord:  m1ke#0003 <87667679+m1ke-adams@users.noreply.github.com>
2021-08-17 17:14:27 +0300

    Update siclon.py

--- siclon.py
+++ siclon.py
@@ -1,6 +1,6 @@
 import requests
 knk=input("site giriniz kopyalanacak: ")
-qwe=requests.get("knk",verify=False)
+qwe=requests.get(knk,verify=False)
 fayil=open("site.txt","w")
 fayil.write(str(qwe.content))
 fayil.close()
