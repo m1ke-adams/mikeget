358c9f902fec8b2881c0725b949ab2b315ac7f08
Discord:  m1ke#0003 <87667679+m1ke-adams@users.noreply.github.com>
2021-08-20 18:02:29 +0300

    Update mikeget.py

--- mikeget.py
+++ mikeget.py
@@ -1,4 +1,13 @@
 import requests
+
+#bu yazılım sahibi belirtilecek şekilde değiştirilebilir veya geliştirilebilir sadece, aksi takdirde dava açılır.
+print("""
+
+┌───── •✧✧• ─────┐
+ -Yapımcı Mike adams 
+└───── •✧✧• ─────┘
+Web Site Open Source Clonner tool v1.0
+""")
 knk=input("site giriniz kopyalanacak: ")
 qwe=requests.get(knk,verify=False)
 fayil=open("site.txt","w")
